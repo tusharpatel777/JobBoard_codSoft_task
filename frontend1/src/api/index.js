63a159558f7395eb62e9812c3b03af176863fcab
tusharpatel777 <tussipatel826@gmail.com>
2025-08-02 19:15:44 +0530

    working

--- frontend1/src/api/index.js
+++ frontend1/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://jobboard-codsoft-task.onrender.com';
+const API_BASE_URL ='https://jobboard-codsoft-task.onrender.com';
 
 const API = axios.create({
   baseURL: `${API_BASE_URL}/api`,
